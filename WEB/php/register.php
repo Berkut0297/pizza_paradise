8f2b62fad983b2e21e0ce3711115a3421e2ff711
Kis Marcell <kismarcell2007@gmail.com>
2025-12-17 13:27:22 +0100

    register backend

---
+++ WEB/php/register.php
@@ -0,0 +1,24 @@
+<?php
+//Load the enviroment.php
+require_once("../../../common/php/environment.php");
+//argument define
+$args = Util::getArgs();
+//db connection
+$db = new Database();
+//define the sql command
+$query = "SELECT `email`,`phone` FROM users;";
+//execute the sql command
+$result = $db->execute($query);
+//check the email is already in the database
+if (isset($result[0]["email"]) && $result[0]["email"] === $args["email"]) 
+  Util::setError("Az email már használt!");
+//check the phone is already in the database
+if (isset($result[0]["phone"]) && $result[0]["phone"] === $args["phone"]) 
+  Util::setError("A Telefon szám már használt!");
+//prepare the insert
+$query = $db->preparateInsert("users",$args);
+//execute the insert to the data base
+$result = $db->execute($query, array_values($args));
+
+$db = null;
+Util::setresponse($result);
